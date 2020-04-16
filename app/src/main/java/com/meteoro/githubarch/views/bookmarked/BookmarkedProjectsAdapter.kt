9f1017c991833be16966d355a6603c2e33ce9db0
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2020-04-16 14:06:35 -0300

    Adicionado tela de repositories salvos

---
+++ app/src/main/java/com/meteoro/githubarch/views/bookmarked/BookmarkedProjectsAdapter.kt
@@ -0,0 +1,42 @@
+package com.meteoro.githubarch.views.bookmarked
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.meteoro.githubarch.R
+import com.meteoro.githubarch.presentation.model.ProjectUI
+import javax.inject.Inject
+
+class BookmarkedProjectsAdapter @Inject constructor() : RecyclerView.Adapter<BookmarkedProjectsAdapter.ViewHolder>() {
+
+    var projectsList: List<ProjectUI> = arrayListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_bookmarked_projects, parent, false)
+        return ViewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int = projectsList.count()
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val project = projectsList[position]
+        holder.ownerNameText.text = project.ownerName
+        holder.projectNameText.text = project.fullName
+
+        Glide.with(holder.itemView.context)
+            .load(project.ownerAvatar)
+            .apply(RequestOptions.circleCropTransform())
+            .into(holder.avatarImage)
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var avatarImage: ImageView = view.findViewById(R.id.image_owner_avatar)
+        var ownerNameText: TextView = view.findViewById(R.id.text_owner_name)
+        var projectNameText: TextView = view.findViewById(R.id.text_project_name)
+    }
+}
--- app/src/main/java/com/meteoro/githubarch/views/bookmarked/BookmarkedProjectsFragment.kt
+++ app/src/main/java/com/meteoro/githubarch/views/bookmarked/BookmarkedProjectsFragment.kt
@@ -1,5 +1,64 @@
 package com.meteoro.githubarch.views.bookmarked
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.meteoro.githubarch.R
+import com.meteoro.githubarch.injection.factory.ViewModelFactory
+import com.meteoro.githubarch.presentation.model.ProjectUI
+import com.meteoro.githubarch.presentation.resource.Resource
+import com.meteoro.githubarch.presentation.resource.ResourceState
+import com.meteoro.githubarch.presentation.view.bookmarked.BookmarkedProjectsViewModel
+import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.fragment_bookmarked_projects.*
+import javax.inject.Inject
 
-class BookmarkedProjectsFragment : Fragment()
+class BookmarkedProjectsFragment : Fragment() {
+
+    @Inject
+    lateinit var adapter: BookmarkedProjectsAdapter
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private lateinit var viewModel: BookmarkedProjectsViewModel
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bookmarked_projects, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupRecyclerView()
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(BookmarkedProjectsViewModel::class.java)
+        viewModel.itemsResources.observe(viewLifecycleOwner, Observer { handleItemsRecource(it) })
+    }
+
+    private fun handleItemsRecource(itemsRes: Resource<List<ProjectUI>>?) {
+        if (itemsRes == null) {
+            return
+        }
+
+        progress.visibility = if (itemsRes.state == ResourceState.LOADING)
+            View.VISIBLE else View.GONE
+
+        adapter.projectsList = itemsRes.data ?: emptyList()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setupRecyclerView() {
+        recycler_projects.layoutManager = LinearLayoutManager(context)
+        recycler_projects.adapter = adapter
+    }
+}
